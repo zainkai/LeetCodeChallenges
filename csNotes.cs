ebbd127a49dd968f15292f359a904a098e023d59
zainkai <turkingtonkevin@gmail.com>
2018-02-13 20:17:10 -0800

    general snippets

--- csNotes.cs
+++ csNotes.cs
@@ -13,3 +13,33 @@ MyClass temp = x ?? new MyClass();
 //  .Where(x => x) returns IEnumerable.
 List<int> ids = new List(genIds());
 List<int> lessThan100 = ids.Where(x => x < 100).ToList();
+ids.add(23423);
+
+
+// Type Conversions
+// String to int
+int x =0;
+if (Int32.TryParse("323123", out x)) { }
+int x = Int32.Parse("23123");
+
+// int to string
+int x = 3324234;
+string str = x.ToString();
+
+//String to char Pretty simple
+string str = "blahsdadsa";
+foreach(char i in str) {
+    //do stuff
+}
+char x = str[2];
+
+// ADT's
+using System.Collections;
+
+Stack<T> stk = new Stack<T>();
+stk.Count;
+stk.Clear();
+stk.Contains('Blah');
+stk.Peek(); // like top
+stk.Pop();
+stk.Push('asdasd');
