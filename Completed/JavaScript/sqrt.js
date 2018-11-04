6a304d21a8ca925c1f37dd89a29894a4fec75f2d
Kevin Turkington <turkingtonkevin@gmail.com>
2018-11-03 22:43:30 -0700

    math hard

---
+++ Completed/JavaScript/sqrt.js
@@ -0,0 +1,18 @@
+function sqrt(x) {
+  let low = 0, high = Number.MAX_SAFE_INTEGER // not x
+  let mid
+
+  while (low <= high) {
+    mid = Math.floor((low+high)/2)
+    if ((mid * mid) === x) return mid
+    if (mid > (x/mid)) {
+      high = mid -1
+    } else { 
+      if((mid+1) > (x /(mid+1))) return mid
+      low = mid +1
+    }
+  }
+  return -1
+}
+
+console.log(sqrt(8))
