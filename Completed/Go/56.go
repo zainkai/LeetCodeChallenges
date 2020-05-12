c4f85e2b10aceb215539007122832b867150254d
zainkai <turkingtonkevin@gmail.com>
2020-05-11 22:05:09 -0700

    update 56

--- Completed/Go/56.go
+++ Completed/Go/56.go
@@ -1,27 +1,27 @@
 import "sort"
 
 func merge(intervals [][]int) [][]int {
-	if len(intervals) == 0 || len(intervals) == 1 {
-		return intervals
-	}
-
-	sort.Slice(intervals, func(i, j int) bool {
-		return intervals[i][0] < intervals[j][0]
-	})
-
-	newInternals := make([][]int, 0)
-	a, b := intervals[0][0], intervals[0][1]
-	for i := 1; i < len(intervals); i++ {
-		c, d := intervals[i][0], intervals[i][1]
-		if c <= b && b < d {
-			b = d
-		} else if c > b {
-			newInternals = append(newInternals, []int{a, b})
-			a = c
-			b = d
-		}
-	}
-	newInternals = append(newInternals, []int{a, b})
-	return newInternals
-
+  if len(intervals) == 0 || len(intervals) == 1 {
+    return intervals
+  }
+  
+  sort.Slice(intervals, func (i, j int) bool {
+    return intervals[i][0] < intervals[j][0]
+  })
+  
+  newInternals := make([][]int, 0)
+  s1, e1 := intervals[0][0], intervals[0][1]
+  for i := 1; i < len(intervals); i++ {
+    s2, e2 := intervals[i][0], intervals[i][1]
+    if s2 <= e1 && e1 < e2 {
+      e1 = e2
+    } else if s2 > e1 {
+      newInternals = append(newInternals, []int{ s1,e1 })
+      s1 = s2
+      e1 = e2
+    }
+  }
+  newInternals = append(newInternals, []int{ s1,e1 })
+  return newInternals
+  
 }
