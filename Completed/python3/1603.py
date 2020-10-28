5ebf5bbd804048769840f38cf96b4eb8d487a77f
Kevin Turkington <turkingtonkevin@gmail.com>
2020-10-27 22:25:06 -0700

    Create 1603.py

---
+++ Completed/python3/1603.py
@@ -0,0 +1,19 @@
+class ParkingSystem:
+    def __init__(self, big: int, medium: int, small: int):
+        self.lot = {
+            1: big,
+            2: medium,
+            3: small,
+        }
+
+    def addCar(self, carType: int) -> bool:
+        if self.lot[carType] == 0:
+            return False
+        
+        self.lot[carType] -=1
+        return True
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
