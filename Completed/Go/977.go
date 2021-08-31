f4f3dc008ec2b07eabced146dc3598e022b4e5f1
Kevin Turkington <turkingtonkevin@gmail.com>
2021-08-31 16:17:22 -0700

    Update 977.go

--- Completed/Go/977.go
+++ Completed/Go/977.go
@@ -1,9 +1,20 @@
-import "sort"
-
-func sortedSquares(A []int) []int {
-    for i, val := range A {
-        A[i] = val* val
+func sortedSquares(nums []int) []int {
+    result := make([]int, len(nums))
+    resCount := len(nums)-1
+    
+    lo, hi := 0, len(nums)-1
+    for lo <= hi {
+        lo2 := nums[lo] * nums[lo]
+        hi2 := nums[hi] * nums[hi]
+        
+        if lo2 > hi2 {
+            result[resCount] = lo2
+            lo++
+        } else {
+            result[resCount] = hi2
+            hi--
+        }
+        resCount--
     }
-    sort.Ints(A)
-    return A
+    return result
 }
