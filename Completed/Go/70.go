c9b83a8101575ff9c11ac35c6dd2a1a7e9fe57e1
Kevin Turkington <turkingtonkevin@gmail.com>
2021-09-21 19:29:44 -0700

    Update 70.go

--- Completed/Go/70.go
+++ Completed/Go/70.go
@@ -10,3 +10,21 @@ func climbStairs(n int) int {
 
 	return m[n]
 }
+
+func climbStairs(n int) int {
+    memo := map[int]int{
+        0: 1,
+        1: 1,
+    }
+    
+    return helper(memo, n)
+}
+
+func helper(memo map[int]int, n int) int {
+    if paths, ok := memo[n]; ok {
+        return paths
+    }
+    
+    memo[n] = helper(memo, n-1) + helper(memo, n-2)
+    return memo[n]
+}
