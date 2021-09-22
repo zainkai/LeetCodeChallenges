e5ca30840b758c45e234cb02b7ea1afd3e595334
Kevin Turkington <turkingtonkevin@gmail.com>
2021-09-21 19:21:00 -0700

    Update 217.go

--- Completed/Go/217.go
+++ Completed/Go/217.go
@@ -1,3 +1,5 @@
+// time O(n) space O(n)
+
 func containsDuplicate(nums []int) bool {
 	m := map[int]struct{}{}
 
@@ -9,4 +11,25 @@ func containsDuplicate(nums []int) bool {
 	}
 
 	return false
+}
+
+// time O(n log n) space O(1)
+import "sort"
+
+func containsDuplicate(nums []int) bool {
+    seenDup := false
+    sort.Slice(nums, func(i, j int) bool {
+        a, b := nums[i], nums[j]
+        if a == b {
+            seenDup = true
+        }
+        
+        if a < b {
+            return true
+        } else {
+            return false
+        }
+    })
+    
+    return seenDup
 }
