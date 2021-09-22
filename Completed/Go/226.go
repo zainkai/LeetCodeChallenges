18d00f78fb98343641cb17d399e6a5b7be76b20e
Kevin Turkington <turkingtonkevin@gmail.com>
2021-09-21 19:14:35 -0700

    Update 226.go

--- Completed/Go/226.go
+++ Completed/Go/226.go
@@ -9,20 +9,49 @@ package LeetCode
  * }
  */
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
 func invertTree(root *TreeNode) *TreeNode {
 	if root == nil {
 		return root
 	}
 
-	temp := invertTree(root.Left)
-	root.Left = invertTree(root.Right)
-	root.Right = temp
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
 
 	return root
+}
+
+// iterative
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+    
+    stk := []*TreeNode{}
+    tmp := root
+    
+    for tmp != nil || len(stk) > 0 {
+        for tmp != nil {
+            // swap
+            tmp.Left, tmp.Right = tmp.Right, tmp.Left
+            
+            stk = append(stk, tmp)
+            tmp = tmp.Left
+        }
+        
+        // top
+        tmp = stk[len(stk)-1]
+        // pop
+        stk = stk[:len(stk)-1]
+        
+        tmp = tmp.Right
+    }
+    
+    return root
 }
