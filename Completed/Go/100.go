f11e25879362100069a1bbb37539f152fa870fec
Kevin Turkington <turkingtonkevin@gmail.com>
2021-09-21 20:51:42 -0700

    Update 100.go

--- Completed/Go/100.go
+++ Completed/Go/100.go
@@ -11,7 +11,10 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
         return true
     } else if p == nil || q == nil {
         return false
-    } else  {
-        return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+    } else if p.Val != q.Val {
+        return false
+    } else {
+        return isSameTree(p.Left,q.Left) &&
+            isSameTree(p.Right,q.Right)
     }
 }
