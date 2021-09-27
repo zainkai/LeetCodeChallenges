b2fb5938dc3ee9e8a4a7aae2c3952b64881c1961
Kevin Turkington <turkingtonkevin@gmail.com>
2021-09-27 12:25:41 -0700

    Update 238.go

--- Completed/Go/238.go
+++ Completed/Go/238.go
@@ -1,24 +1,36 @@
 func productExceptSelf(nums []int) []int {
-	if len(nums) < 0 {
-		return nums
-	}
-
-	output, product := make([]int, len(nums)), 1
-	for i := len(nums) - 1; i >= 0; i-- {
-		product *= nums[i]
-		output[i] = product
-	}
-
-	product = 1
-	for i, val := range nums {
-		rVal := 1
-		if i+1 < len(nums) {
-			rVal = output[i+1]
-		}
-
-		output[i] = product * rVal
-		product *= val
-	}
-
-	return output
+    if len(nums) <= 1 {
+        return nums
+    }
+    
+    res := make([]int, len(nums))
+    
+    L := make([]int, len(nums))
+    tmp := 1
+    for i := 0; i < len(nums); i++ {
+        tmp *= nums[i]
+        L[i] = tmp
+    }
+    
+    R:= make([]int, len(nums))
+    tmp = 1
+    for i := len(nums)-1; i >=0; i-- {
+        tmp *= nums[i]
+        R[i] = tmp
+    }
+    
+    for i := range nums {
+        left := 1
+        if i-1 >= 0 {
+            left = L[i-1]
+        }
+        
+        right := 1
+        if i+1 < len(nums) {
+            right = R[i+1]
+        }
+        
+        res[i] = left * right
+    }
+    return res
 }
