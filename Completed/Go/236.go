a2acc2e685eb4e39b89093ec6ad0ff2795662c75
Kevin Turkington <turkingtonkevin@gmail.com>
2021-09-27 13:04:48 -0700

    Update 236.go

--- Completed/Go/236.go
+++ Completed/Go/236.go
@@ -34,3 +34,54 @@ func dfs(root, p, q *TreeNode, lca **TreeNode) bool {
 	}
 	return cnt > 0
 }
+
+
+
+///********* new
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    pMap := map[*TreeNode]bool{}
+    qMap := map[*TreeNode]bool{}
+
+    hasElm(root, p, pMap)
+    hasElm(root, q, qMap)
+
+    return helper(root, pMap, qMap)
+}
+
+func hasElm(root, target *TreeNode, visited map[*TreeNode]bool) bool {
+    if root == nil {
+        return false
+    } else if root == target {
+        visited[root] = true
+        return true
+    }
+
+    visited[root] = hasElm(root.Left, target, visited) || hasElm(root.Right, target, visited)
+    return visited[root]
+}
+
+func helper(root *TreeNode, pMap, qMap map[*TreeNode]bool) *TreeNode {
+    if root == nil {
+        return nil
+    }
+
+    if r := helper(root.Left, pMap, qMap); r != nil {
+        return r
+    } else if r := helper(root.Right, pMap, qMap); r != nil {
+        return r
+    }
+
+    if pMap[root] && qMap[root] {
+        return root
+    }
+    return nil
+}
