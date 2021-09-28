06e7f4d020f642a9fc1d11c1070d7572a8a8a23f
Kevin Turkington <turkingtonkevin@gmail.com>
2021-09-27 21:53:13 -0700

    Update 1165.go

--- Completed/Go/1165.go
+++ Completed/Go/1165.go
@@ -1,22 +1,19 @@
 func calculateTime(keyboard string, word string) int {
-	m := map[rune]int{}
-	for i, key := range keyboard {
-		m[key] = i
-	}
-
-	lastIdx := 0
-	result := 0
-	for _, c := range word {
-		result += abs(m[c] - lastIdx)
-		lastIdx = m[c]
-	}
-
-	return result
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
+    keyboardCost := map[rune]int{}
+    for i, key := range keyboard {
+        keyboardCost[key] = i
+    }
+    
+    res := 0
+    lastKey := 0
+    for _, key := range word {
+        newKey := keyboardCost[key]
+        if newKey < lastKey {
+            res += lastKey - newKey
+        } else {
+            res += newKey - lastKey
+        }
+        lastKey = newKey
+    }
+    return res
 }
