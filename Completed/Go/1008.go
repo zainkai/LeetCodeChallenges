d4e9ed5ee416af6a78df7df02c2b2ce99464a108
Kevin Turkington <turkingtonkevin@gmail.com>
2021-10-12 20:44:26 -0700

    Update 1008.go

--- Completed/Go/1008.go
+++ Completed/Go/1008.go
@@ -6,29 +6,24 @@
  *     Right *TreeNode
  * }
  */
- const (
-    MIN = -1 << 63
-    MAX = 1 << 63-1
-)
-
-
 func bstFromPreorder(preorder []int) *TreeNode {
-    i := 0
-    return dfs(preorder, &i, MIN, MAX)
+    return helper(&preorder, -1<<63, 1<<63-1)
 }
 
-func dfs(nums []int, i *int, min int, max int) *TreeNode {
-    if *i > len(nums)-1 {
+func helper(preorder *[]int, min, max int) *TreeNode {
+    if len(*preorder) == 0 {
         return nil
-    } else if val := nums[*i]; val < min || val > max {
+    } 
+    val := (*preorder)[0]
+    if val < min || val > max {
         return nil
+    } else {
+        *preorder = (*preorder)[1:]
     }
     
-    head := &TreeNode{ nums[*i], nil, nil }
-    *i += 1
-    
-    head.Left = dfs(nums, i, min, head.Val)
-    head.Right = dfs(nums, i, head.Val, max)
-    
-    return head
+    return &TreeNode{
+        Val: val,
+        Left: helper(preorder, min, val),
+        Right: helper(preorder, val, max),
+    }
 }
