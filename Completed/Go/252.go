a95133a66fbd4123f5b4902179847a10a3035202
Kevin Turkington <turkingtonkevin@gmail.com>
2021-10-21 11:45:18 -0700

    Update 252.go

--- Completed/Go/252.go
+++ Completed/Go/252.go
@@ -7,23 +7,29 @@ func canAttendMeetings(intervals [][]int) bool {
         return true
     }
     
-    sort.Slice(intervals, func(i,j int) bool {
-        if intervals[i][0] != intervals[j][0] {
-            return intervals[i][0] < intervals[j][0]
-        }
-        return intervals[i][1] < intervals[j][1]
+    sort.Slice(intervals, func(i, j int) bool {
+        return intervals[i][0] < intervals[j][0]
     })
     
-    _, e1 := intervals[0][0], intervals[0][1]
+    sTmp, eTmp := intervals[0][0], intervals[0][1]
     for i := 1; i < len(intervals); i++ {
-        s2, e2 := intervals[i][0], intervals[i][1]
-        
-        if s2 < e1 {
+        if isOverlap([]int{sTmp, eTmp}, intervals[i]) {
             return false
-        } else if s2 >= e1 {
-            e1 = e2
+        } else {
+            s, e := intervals[i][0], intervals[i][1]
+            sTmp, eTmp = s, e
         }
     }
     
     return true
+}
+
+func isOverlap(inv1, inv2 []int) bool {
+    s1, e1 := inv1[0], inv1[1]
+    s2, _ := inv2[0], inv2[1]
+    
+    if s2 >= s1 && s2 < e1 {
+        return true
+    }
+    return false
 }
