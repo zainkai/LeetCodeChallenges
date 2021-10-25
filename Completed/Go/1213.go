35e350577a72aea708e6755ad4704c05d1b5ba87
Kevin Turkington <turkingtonkevin@gmail.com>
2021-10-25 13:53:29 -0700

    Update 1213.go

--- Completed/Go/1213.go
+++ Completed/Go/1213.go
@@ -1,22 +1,25 @@
-import (
-    "sort"
-)
+import "sort"
 
 func arraysIntersection(arr1 []int, arr2 []int, arr3 []int) []int {
-    m := map[int]int{}
-    for _, arr := range [][]int{arr1,arr2,arr3} {
-        for _, v := range arr {
-            m[v]++
-        }
-    }
-    
+    m1, m2, m3 := mapping(arr1), mapping(arr2), mapping(arr3)
     res := []int{}
-    for key, val := range m {
-        if val == 3 {
-            res = append(res, key)
+    
+    for k := range m1 {
+        if m2[k] && m3[k] {
+            res = append(res, k)
         }
     }
     
     sort.Ints(res)
     return res
+}
+
+func mapping(a []int) map[int]bool {
+    m := map[int]bool{}
+    
+    for _, v := range a {
+        m[v] = true
+    }
+    
+    return m
 }
