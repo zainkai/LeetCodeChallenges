c9d334b6f3aa15ff33c2d4953d7cd0b6c6c77425
Kevin Turkington <turkingtonkevin@gmail.com>
2021-10-24 22:22:59 -0700

    Update 314.go

--- Completed/Go/314.go
+++ Completed/Go/314.go
@@ -6,82 +6,56 @@
  *     Right *TreeNode
  * }
  */
- import "container/list"
+type frame struct {
+    node *TreeNode
+    r int
+}
 
- type frame struct {
-	 node     *TreeNode
-	 vertical int
- }
- 
- func verticalOrder(root *TreeNode) [][]int {
-	 q := NewQueue()
-	 if root != nil {
-		 q.Enqueue(frame{root, 0})
-	 } else {
-		 return [][]int{}
-	 }
- 
-	 vertMap := map[int][]int{}
-	 leftMost, rightMost := 0, 0
- 
-	 for q.Len() > 0 {
-		 f := q.Dequeue().(frame)
-		 vertMap[f.vertical] = append(vertMap[f.vertical], f.node.Val)
- 
-		 if f.vertical < leftMost {
-			 leftMost = f.vertical
-		 } else if f.vertical > rightMost {
-			 rightMost = f.vertical
-		 }
- 
-		 if f.node.Left != nil {
-			 q.Enqueue(frame{f.node.Left, f.vertical - 1})
-		 }
-		 if f.node.Right != nil {
-			 q.Enqueue(frame{f.node.Right, f.vertical + 1})
-		 }
-	 }
- 
-	 ans := [][]int{}
-	 for i := leftMost; i <= rightMost; i++ {
-		 ans = append(ans, vertMap[i])
-	 }
-	 return ans
- }
- 
- // Queue ...
- type Queue struct {
-	 list *list.List
- }
- 
- // NewQueue ...
- func NewQueue() *Queue {
-	 return &Queue{
-		 list: list.New(),
-	 }
- }
- 
- // Len ...
- func (q *Queue) Len() int {
-	 return q.list.Len()
- }
- 
- // Enqueue ...
- func (q *Queue) Enqueue(val interface{}) {
-	 q.list.PushBack(val)
- }
- 
- // Dequeue ...
- func (q *Queue) Dequeue() interface{} {
- 
-	 ele := q.list.Front()
-	 defer q.list.Remove(ele)
- 
-	 return ele.Value
- }
- 
- // Top ...
- func (q *Queue) Top() interface{} {
-	 ele := q.list.Front()
-	 return ele.Value
- }
+func verticalOrder(root *TreeNode) [][]int {
+    res := [][]int{}
+    if root == nil {
+        return res
+    }
+    
+    q := []frame{
+        {root, 0},
+    }
+    leftMostVert := 0
+    rightMostVert := 0
+    
+    vertMap := map[int][]int{}
+    for len(q) > 0 {
+        node, r := q[0].node, q[0].r
+        q = q[1:]
+        
+        leftMostVert = min(leftMostVert, r)
+        rightMostVert = max(rightMostVert, r)
+        
+        vertMap[r] = append(vertMap[r], node.Val)
+        if node.Left != nil {
+            q = append(q, frame{node.Left, r-1})
+        }
+        if node.Right != nil {
+            q = append(q, frame{node.Right, r+1})
+        }
+    }
+    
+    for i:=leftMostVert; i <= rightMostVert; i++ {
+        res = append(res, vertMap[i])
+    }
+    return res
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func max(a,b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
