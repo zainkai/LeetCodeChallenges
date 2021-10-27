ea54bc3b1ee9521840c771bc659bc2404a3c933a
Kevin Turkington <turkingtonkevin@gmail.com>
2021-10-27 09:48:39 -0700

    Update 339.go

--- Completed/Go/339.go
+++ Completed/Go/339.go
@@ -39,3 +39,52 @@ func dfs(nl []*NestedInteger, depth int) int {
 
 	return sum
 }
+
+// bfs
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ * // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (n NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (n NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (n *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (n NestedInteger) GetList() []*NestedInteger {}
+ */
+
+func depthSum(nestedList []*NestedInteger) int {
+    res := 0
+    depth := 1
+    q := nestedList
+    
+    for len(q) > 0 {
+        size := len(q)
+        for i := 0; i < size; i++ {
+            top := q[0]
+            q = q[1:]
+            if top.IsInteger() {
+                res += (depth * top.GetInteger())
+            } else {
+                q = append(q, top.GetList()...)
+            }
+        }
+        depth++
+    }
+    
+    return res
+}
