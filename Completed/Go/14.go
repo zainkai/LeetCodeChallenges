535cfd1a9572221b4a01d304c894843a8e22597d
Kevin Turkington <turkingtonkevin@gmail.com>
2021-11-04 14:17:52 -0700

    Update 14.go

--- Completed/Go/14.go
+++ Completed/Go/14.go
@@ -1,25 +1,22 @@
 func longestCommonPrefix(strs []string) string {
-    if len(strs) == 0 {
-        return ""
-    } else if len(strs) == 1 {
-        return strs[0]
-    }
+    i := 0
     
-    idx := 0
     topLoop:
-    for {
-        for i := 1; i < len(strs); i++ {
-            word1, word2 := strs[i-1], strs[i]
-            if idx >= len(word1) || idx >= len(word2) {
-                break topLoop
-            }
-            
-            if word1[idx] != word2[idx] {
+    for ;i < len(strs[0]); i++ {
+        ch := strs[0][i]
+        for _, str := range strs {
+            if i >= len(str) || str[i] != ch { 
                 break topLoop
             }
         }
-        idx++
     }
     
-    return strs[0][0:idx]
+    return strs[0][:i]
+}
+
+func min(a,b int) int {
+    if a < b {
+        return a
+    }
+    return b
 }
