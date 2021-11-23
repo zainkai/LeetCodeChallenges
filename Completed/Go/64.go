eab279ebe8b624e1227f722babd8b044ce4119c0
Kevin Turkington <turkingtonkevin@gmail.com>
2021-11-22 21:33:47 -0800

    Update 64.go

--- Completed/Go/64.go
+++ Completed/Go/64.go
@@ -1,48 +1,37 @@
+type coord struct {
+    x,y int
+}
+
 func minPathSum(grid [][]int) int {
-	n, m := len(grid), 0
-	if n > 0 {
-		m = len(grid[0])
-	}
-
-	for i, row := range grid {
-		for j, val := range row {
-			if i == 0 && j == 0 {
-				continue
-			}
-
-			left := 1<<63 - 1
-			if isValidSpace(n, m, i, j-1) {
-				left = grid[i][j-1] + val
-			}
-
-			top := 1<<63 - 1
-			if isValidSpace(n, m, i-1, j) {
-				top = grid[i-1][j] + val
-			}
-
-			grid[i][j] = min(left, top)
-		}
-	}
-
-	if n > 0 && m > 0 {
-		return grid[n-1][m-1]
-	}
-	return 0
+    memo := map[coord]int{
+        coord{ len(grid)-1, len(grid[0])-1 }:grid[len(grid)-1][len(grid[0])-1],
+        
+    }
+    return helper(grid, coord{0,0}, memo)
 }
 
-func isValidSpace(n, m, i, j int) bool {
-	if i < 0 || j < 0 {
-		return false
-	} else if i >= n || j >= m {
-		return false
-	} else {
-		return true
-	}
+func helper(grid [][]int, curr coord, memo map[coord]int) int {
+    if res, ok := memo[curr]; ok {
+        return res
+    } else if curr.x >= len(grid) || curr.y >= len(grid[curr.x]) {
+        return 1<<63-1
+    }
+    
+    memo[curr] = 1<<63-1
+    if res := helper(grid, coord{curr.x+1, curr.y}, memo); res != 1<<63-1 {
+        memo[curr] = min(memo[curr], res)
+    }
+    if res := helper(grid, coord{curr.x, curr.y+1}, memo); res != 1<<63-1 {
+        memo[curr] = min(memo[curr], res)
+    }
+    
+    memo[curr] += grid[curr.x][curr.y]
+    return memo[curr]
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+func min(a,b int) int {
+    if a < b {
+        return a
+    }
+    return b
 }
