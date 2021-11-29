b310fb8fffdb57aacfa83f26720bbccabd54f8d8
Kevin Turkington <turkingtonkevin@gmail.com>
2021-11-28 17:39:38 -0800

    Update 438.go

--- Completed/Go/438.go
+++ Completed/Go/438.go
@@ -1,42 +1,24 @@
 func findAnagrams(s string, p string) []int {
-    pMap, pCount := map[byte]int{}, 0
-    for _, r := range p {
-        pMap[byte(r)]++
-        pCount++
+    pMap := getCharArr(p)
+    res := []int{}
+    
+    for i := 0; i < len(s) - len(p)+1; i++ {
+        tmp := s[i:i+len(p)]
+        
+        if pMap == getCharArr(tmp) {
+            res = append(res, i)
+        }
     }
     
-    result := []int{}
-    sMap, sCount := map[byte]int{}, 0
-    for i := 0; i < len(s); i++ {
-        if sCount <= pCount {
-            sMap[s[i]]++
-            sCount++
-        }
-        if sCount > pCount {
-            char := s[i-len(p)]
-            sMap[char]--
-            if sMap[char] == 0 {
-                delete(sMap, char)
-            }
-            sCount--
-        }
-        
-        if compareMaps(pMap, sMap) {
-            result = append(result, i-len(p)+1)
-        }
-    }
-    return result
+    return res
 }
 
-func compareMaps(a, b map[byte]int) bool {
-    if len(a) != len(b) {
-        return false
+func getCharArr(s string) [26]int {
+    res := [26]int{}
+    for _, elm := range s {
+        idx := int(elm - 'a')
+        res[idx]++
     }
     
-    for key := range a {
-        if b[key] != a[key] {
-            return false
-        }
-    }
-    return true
+    return res
 }
