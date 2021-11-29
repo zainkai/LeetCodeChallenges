9a1eaa3c21aadeb156e542841a3bc653f1b42d6f
Kevin Turkington <turkingtonkevin@gmail.com>
2021-11-29 13:23:43 -0800

    Update 973.go

--- Completed/Go/973.go
+++ Completed/Go/973.go
@@ -1,25 +1,19 @@
 import (
-	"math"
-	"sort"
+    "sort"
 )
 
-func kClosest(points [][]int, K int) [][]int {
-	sort.Slice(points, func(a, b int) bool {
-		return distFromOrigin(points[a]) < distFromOrigin(points[b])
-	})
 
-	vals := [][]int{}
-	for i := 0; i < K; i++ {
-		vals = append(vals, points[i])
-	}
-
-	return vals
+func kClosest(points [][]int, k int) [][]int {
+    sort.Slice(points, func(i,j int) bool {
+        return getDistOrigin(points[i]) < getDistOrigin(points[j])
+    })
+    
+    return points[:k]
 }
 
-func distFromOrigin(point []int) float64 {
-	x, y := 0-point[0], 0-point[1]
-	x *= x
-	y *= y
-
-	return math.Sqrt(float64(x + y))
+func getDistOrigin(point []int) int {
+    x := point[0] * point[0]
+    y := point[1] * point[1]
+    
+    return x+y
 }
