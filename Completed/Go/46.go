d355d43f65e2cf57df27e776378373a31c98c45b
Kevin Turkington <turkingtonkevin@gmail.com>
2021-11-28 19:03:39 -0800

    Update 46.go

--- Completed/Go/46.go
+++ Completed/Go/46.go
@@ -1,49 +1,25 @@
-// First Solution
-// func permute(nums []int) [][]int {
-//   res := make([][]int, 0)
-//   helper(nums, []int{}, &res)
-
-//   return res
-// }
-
-// func helper(nums []int, output []int, res *[][]int) {
-//   if len(nums) == 0 {
-//     *res = append(*res, output)
-//   }
-
-//   for i, val := range nums {
-//     tempNums := make([]int, 0)
-//     tempNums = append(tempNums, nums[:i]...)
-//     tempNums = append(tempNums, nums[i+1:]...)
-
-//     tempOutput := make([]int, len(output))
-//     copy(tempOutput, output)
-//     tempOutput = append(tempOutput, val)
-
-//     helper(tempNums, tempOutput, res)
-//   }
-// }
-
-// Better Solution 2nd iteration
 func permute(nums []int) [][]int {
-	res := make([][]int, 0)
-	helper(0, nums, &res)
-
-	return res
+    res := [][]int{}
+    helper(nums, 0, &res)
+    
+    return res
 }
 
-func helper(i int, nums []int, res *[][]int) {
-	if i == len(nums)-1 {
-		temp := make([]int, len(nums))
-		copy(temp, nums)
+func helper(arr []int, idx int, res *[][]int) {
+    if idx == len(arr) {
+        *res = append(*res, deepCopy(arr))
+    }
+    
+    for i := idx; i < len(arr); i++ {
+        arr[idx], arr[i] = arr[i], arr[idx]
+        helper(arr, idx+1, res)
+        arr[idx], arr[i] = arr[i], arr[idx]
+    }
+} 
 
-		*res = append(*res, temp)
-		return
-	}
-
-	for j := i; j < len(nums); j++ {
-		nums[i], nums[j] = nums[j], nums[i]
-		helper(i+1, nums, res)
-		nums[j], nums[i] = nums[i], nums[j]
-	}
+func deepCopy(arr []int) []int {
+    tmp := make([]int, len(arr))
+    copy(tmp, arr)
+    
+    return tmp
 }
