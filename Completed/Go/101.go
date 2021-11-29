ff1d93418acfaeb375d923314cdf648598962469
Kevin Turkington <turkingtonkevin@gmail.com>
2021-11-28 17:18:21 -0800

    Update 101.go

--- Completed/Go/101.go
+++ Completed/Go/101.go
@@ -7,15 +7,17 @@
  * }
  */
 func isSymmetric(root *TreeNode) bool {
-	return helper(root, root)
+    return helper(root, root)
 }
 
-func helper(a, b *TreeNode) bool {
-	if a == nil && b == nil {
-		return true
-	} else if a == nil || b == nil {
-		return false
-	}
-
-	return a.Val == b.Val && helper(a.Left, b.Right) && helper(b.Left, a.Right)
+func helper(r1, r2 *TreeNode) bool {
+    if r1 == nil && r2 == nil {
+        return true
+    } else if r1 == nil || r2 == nil {
+        return false
+    } else if r1.Val != r2.Val {
+        return false
+    }
+    
+    return helper(r1.Left, r2.Right) && helper(r1.Right, r2.Left)
 }
