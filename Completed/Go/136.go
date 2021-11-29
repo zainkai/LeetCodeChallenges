818b0cd5ec6f99a2e4f2952771fa3ead99a8c237
Kevin Turkington <turkingtonkevin@gmail.com>
2021-11-28 17:15:15 -0800

    Update 136.go

--- Completed/Go/136.go
+++ Completed/Go/136.go
@@ -1,24 +1,8 @@
 func singleNumber(nums []int) int {
-	m := map[int]int{}
-
-	for _, num := range nums {
-		m[num] += 1
-	}
-
-	for key, val := range m {
-		if val == 1 {
-			return key
-		}
-	}
-
-	return -1
-}
-
-func singleNumber(nums []int) int {
-	res := 0
-	for _, v := range nums {
-		res = res ^ v
-	}
-
-	return res
+    res := 0
+    for _, elm := range nums {
+        res = res ^ elm
+    }
+    
+    return res
 }
